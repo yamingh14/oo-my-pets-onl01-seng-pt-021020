728e31bc057683dfc49aeb0c23ee23bd1ef7f549
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-19 05:23:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,14 @@
 class Dog
-  # code goes here
-  attr_accessor :mood
+attr_accessor :mood, :owner
   attr_reader :name
-
-  def initialize(name)
+  @@all = []
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+  def self.all
+    @@all
   end
 end
